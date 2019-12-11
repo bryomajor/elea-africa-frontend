d455df81939b01d86e1ee23763d5c10915d767ab
Brian Otieno <bryomajor@gmail.com>
2019-12-11 11:52:44 +0300

    create a service that consumes rest API from django and also create the nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+import { ApiService } from './api.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ApiService]
 })
 export class AppComponent {
-  title = 'crud';
+  events = [{title: 'Good Deeds'}, {description: 'Sample description'}];
+
+  constructor(private api:ApiService) {
+    this.getEvents();
+  }
+  getEvents = () => {
+    this.api.getAllEvents().subscribe(
+      data => {
+        this.events = data;
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
 }
