e9b8143fe915f526fe9c48381f5348df2238bf32
Brian Otieno <bryomajor@gmail.com>
2020-01-01 14:25:32 +0300

    include a method that subscribes to the getevent observable.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,11 @@
-import { ApiService } from './api.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ApiService]
 })
 export class AppComponent {
-  events = [{title: 'Good Deeds'}, {description: 'Sample description'}];
 
-  constructor(private api:ApiService) {
-    this.getEvents();
-  }
-  getEvents = () => {
-    this.api.getAllEvents().subscribe(
-      data => {
-        this.events = data;
-      },
-      error => {
-        console.log(error)
-      }
-    )
-  }
+  constructor() {}
 }
