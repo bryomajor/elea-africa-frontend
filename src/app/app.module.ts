e309f77cf1a26c85bef2653a4bddd1824d460659
Brian Otieno <bryomajor@gmail.com>
2020-01-01 14:36:57 +0300

    declare created components and import external modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav/nav.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { IntroComponent } from './home/intro/intro.component';
+import { CaseStudyComponent } from './home/case-study/case-study.component';
+import { FeaturedComponent } from './home/featured/featured.component';
+import { ContactComponent } from './contact/contact.component';
+import { IndexComponent } from './home/index/index.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    IntroComponent,
+    CaseStudyComponent,
+    FeaturedComponent,
+    ContactComponent,
+    IndexComponent,
+    NotFoundComponent,
+    EventDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AngularFontAwesomeModule,
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
