16459e78a933b9793e9eca76624d7f74d61afa28
Brian Otieno <bryomajor@gmail.com>
2020-01-01 14:39:14 +0300

    create event class.

---
+++ src/app/event-class/event.ts
@@ -0,0 +1,4 @@
+export class Event {
+
+    constructor(public id: number, public title: string, public location: string, public date: string, public description: string, public thumbnail: File){}
+}
