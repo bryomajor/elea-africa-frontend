407888c29c9f51011857f95d6da5da9196aff9ff
Brian Otieno <bryomajor@gmail.com>
2020-01-01 14:24:17 +0300

    define routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { IndexComponent } from './home/index/index.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: IndexComponent},
+  { path: '', redirectTo: "/home", pathMatch: "full" },
+  { path: 'event/:id', component: EventDetailComponent},
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
