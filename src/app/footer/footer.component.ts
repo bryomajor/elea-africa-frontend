12f0f1b9cf27ded1d5ef058ddb49a86d69731416
Brian Otieno <bryomajor@gmail.com>
2020-01-02 09:50:02 +0300

    make subscription form functional.

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,8 @@
+import { ApiService } from './../api.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +10,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  sendingRequest = false
+  formSiteFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private api: ApiService, private router: Router) { }
 
   ngOnInit() {
+    this.formSiteFormGroup = new FormGroup({
+      email: new FormControl('', [Validators.required]),
+    })
   }
+
+  onSubscribe(event: any) {
+    this.sendingRequest = true
+    console.log(this.formSiteFormGroup.value);
+    const subscriptionFormData = new FormData()
+    subscriptionFormData.append('email', this.formSiteFormGroup.value.email)
+
+    if (this.formSiteFormGroup.invalid) return
+
+    this
+      .api
+      .submitSubscription(subscriptionFormData)
+      .pipe(first())
+      .subscribe(
+        (data: any) => {
+          console.log(data)
+        },
+        (error: any) => {
+          console.log({error})
+          this.sendingRequest = false
+        }
+      )
+  }
+
+  // onSubmit(form: NgForm) {
+  //   this.api.save(form.value).subscribe(
+  //     (data: any) => {
+
+  //       console.log(data);
+  //       // this.contactForm.reset();
+  //     },
+  //     (error: any) => console.log(error)
+  //   );
+  // }
 
 }
